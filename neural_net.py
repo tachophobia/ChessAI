f874e85d7cd3925808c188efd5014a61191beabf
Nikita Lugovoy <niklugovoy@gmail.com>
2024-09-21 18:32:14 -0400

    fixed board mirroring

--- neural_net.py
+++ neural_net.py
@@ -6,49 +6,59 @@ from torch.cuda.amp.autocast_mode import autocast
 
 import numpy as np
 import chess
-import os
 import random
 
+rank_map = {
+    '1': '8', '2': '7', '3': '6', '4': '5',
+    '5': '4', '6': '3', '7': '2', '8': '1'
+}
+
+def mirror_uci(move):
+        move = [*move]
+        move[1], move[3] = rank_map[move[1]], rank_map[move[3]]
+        return ''.join(move)
+
 ACTION_SPACE = open('chess_possible_moves.txt', 'r').read().splitlines()
+MIRRORED_ACTIONS = [*map(mirror_uci, ACTION_SPACE)]
+TRANSITIONS = [ACTION_SPACE.index(a) for a in MIRRORED_ACTIONS]
 
 class Featurizer:
     def __init__(self):
-        self.piece_map = {
-            'p': 0, 'n': 1, 'b': 2, 'r': 3, 'q': 4, 'k': 5,
-            'P': 6, 'N': 7, 'B': 8, 'R': 9, 'Q': 10, 'K': 11
-        }
+        self.symbols = ['P', 'R', 'K', 'B', 'Q', 'K', 'p', 'r', 'k', 'b', 'q', 'k']
 
     def featurize(self, state):
+        matrices = []
         board = chess.Board(state)
-        state = []
+        if not board.turn:
+            board = board.mirror()
 
-        for piece_symbol in self.piece_map:
+        for symbol in self.symbols:
             matrix = np.zeros((8, 8), dtype=int)
             for square in chess.SQUARES:
                 piece = board.piece_at(square)
-                if piece and piece.symbol() == piece_symbol:
+                if piece and piece.symbol() == symbol:
                     i, j = np.unravel_index(square, (8, 8))
                     matrix[i][j] = 1
-            state.append(matrix)
+            matrices.append(matrix)
 
-        side_to_move = np.ones((8, 8), dtype=int) if board.turn == chess.WHITE else np.zeros((8, 8), dtype=int)
-        state.append(side_to_move)
-
-        castling_white = np.ones((8, 8), dtype=int) if board.has_kingside_castling_rights(chess.WHITE) else np.zeros((8, 8), dtype=int)
-        state.append(castling_white)
-        castling_black = np.ones((8, 8), dtype=int) if board.has_kingside_castling_rights(chess.BLACK) else np.zeros((8, 8), dtype=int)
-        state.append(castling_black)
+        ones = np.ones((8, 8), dtype=int)
+        zeros = np.zeros((8, 8), dtype=int)
+        matrices.append(ones if board.has_kingside_castling_rights(chess.WHITE) else zeros)
+        matrices.append(ones if board.has_queenside_castling_rights(chess.WHITE) else zeros)
+        matrices.append(ones if board.has_kingside_castling_rights(chess.BLACK) else zeros)
+        matrices.append(ones if board.has_queenside_castling_rights(chess.BLACK) else zeros)
 
         en_passant = np.zeros((8, 8), dtype=int)
         if board.ep_square is not None:
             i, j = np.unravel_index(board.ep_square, (8, 8))
             en_passant[i][j] = 1
-        state.append(en_passant)
+        matrices.append(en_passant)
 
         halfmove_clock = np.full((8, 8), board.halfmove_clock, dtype=int)
-        state.append(halfmove_clock)
+        matrices.append(halfmove_clock)
 
-        return np.stack(state, axis=0)
+        matrices = np.stack(matrices, axis=0)
+        return matrices
 
 class ResidualBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
@@ -70,7 +80,7 @@ class NeuralNetwork(nn.Module):
     def __init__(self, lr=1e-3, residual_blocks=20, batch_size=64):
         super(NeuralNetwork, self).__init__()
         
-        self.initial_conv = nn.Conv2d(17, 64, kernel_size=3, padding=1)
+        self.initial_conv = nn.Conv2d(18, 64, kernel_size=3, padding=1)
         self.initial_batchnorm = nn.BatchNorm2d(64)
         
         self.residual_blocks = nn.Sequential(
@@ -112,7 +122,11 @@ class NeuralNetwork(nn.Module):
     def evaluate(self, x):
         self.eval()
         with torch.no_grad():
-            return self(x)
+            v, p = self(x)
+            p = p.tolist()[0]
+            if x.split()[1] == 'b':
+                p = sorted(p, key=lambda a: TRANSITIONS[p.index(a)])
+            return v, p
     
     def save_model(self, filename):
         torch.save(self.state_dict(), filename)
@@ -130,6 +144,9 @@ class NeuralNetwork(nn.Module):
             total_loss = 0.
             
             for state, policy, reward in batch:
+                if state.split()[1] == 'b':
+                    # policy action probs are in white basis, convert to black basis
+                    policy = sorted(policy, key=lambda a: TRANSITIONS.index(policy.index(a)))
                 with autocast():
                     predicted_v, predicted_p = self(state)
                     policy = torch.tensor(policy).to(self.device)
@@ -155,14 +172,23 @@ class NeuralNetwork(nn.Module):
         return np.mean(losses)
     
     def choose_move(self, state):
+        mirrored = False
+        if not state.turn:
+            state = state.mirror()
+            mirrored = True
         self.eval()
         with torch.no_grad():
             _, p = self(state.fen())
             p = p.tolist()[0]
-            legal_moves = [*map(str, state.legal_moves)]
-            p = [(p, a) for p, a in zip(p, ACTION_SPACE) if a in legal_moves]
-            weights = [p for p, _ in p]
-            actions = [a for _, a in p]
+            legal_moves = {*map(str, state.legal_moves)}
+            action_space = [ACTION_SPACE, MIRRORED_ACTIONS][mirrored]
+            p = [(p, a) for p, a in zip(p, action_space) if a in legal_moves]
+            weights = [p[0] for p in p]
+            actions = [p[1] for p in p]
             if sum(weights) == 0:
-                return random.choice(actions)
-            return random.choices(actions, weights=weights)[0]
+                weights = [1] * len(weights)
+            action = random.choices(actions, weights=weights)[0]
+            if mirrored:
+                return mirror_uci(action)
+            return action
+        
